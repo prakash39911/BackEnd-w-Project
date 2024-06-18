3e02bbe39bfadf8e9aa609b8c4ef4fc132984d48
prakash39911 <prakash39911@gmail.com>
2024-06-18 17:45:49 +0530

    small changes

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,1 +1,17 @@
-class apiError extends Error {}
+class apiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went Wrong",
+    errors = [],
+    statch = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null; // learn from chat gpt
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+  }
+}
+
+export { apiError };
